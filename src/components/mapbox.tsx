427c87217bd5040fa6d6fd957302b71c3ba8f0ba
Walker <83209083+walkerhsu@users.noreply.github.com>
2024-09-30 20:02:20 +0800

    ✨ Add drawer on the left and fix linter problems (#2)

--- src/components/mapbox.tsx
+++ src/components/mapbox.tsx
@@ -1,33 +1,40 @@
-'use client';
+"use client";
 
-import 'mapbox-gl/dist/mapbox-gl.css';
-import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
-import { useCallback, useRef, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { MapRef, Marker } from 'react-map-gl';
-import styles from '../styles/login.module.css';
-import { MapMouseEvent } from 'mapbox-gl';
-import MapGL from 'react-map-gl';
-import mapboxgl from 'mapbox-gl';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import "mapbox-gl/dist/mapbox-gl.css";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import { useCallback, useRef, useState } from "react";
+// import { useRouter } from 'next/navigation';
+import { MapRef, Marker } from "react-map-gl";
+import styles from "../styles/login.module.css";
+import { MapMouseEvent } from "mapbox-gl";
+import MapGL from "react-map-gl";
+import mapboxgl from "mapbox-gl";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
 const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN!;
 
+type Viewport = {
+  latitude: number;
+  longitude: number;
+  zoom: number | undefined;
+};
+
 export default function Mapbox() {
   const [location, setLocation] = useState({ lat: 0, lng: 0 });
-  const [mapInstance, setMapInstance] = useState<MapRef | null>(null);
-  const [showMap, setShowMap] = useState(false);
+  // const [mapInstance, setMapInstance] = useState<MapRef | null>(null);
+  const [showMarker, setShowMarker] = useState<boolean>(false);
+  // const [showMap, setShowMap] = useState(true);
 
   const [viewport, setViewport] = useState({
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
+    latitude: 25.13680057687235,
+    longitude: 121.50427011487547,
+    zoom: 15,
   });
   const mapRef = useRef<MapRef | null>(null);
-  const geocoderContainerRef = useRef<HTMLDivElement>(null);
+  // const geocoderContainerRef = useRef<HTMLDivElement>(null);
 
   const handleViewportChange = useCallback(
-    (newViewport: any) => {
+    (newViewport: Viewport) => {
       setViewport({
         ...viewport,
         latitude: newViewport.latitude,
@@ -47,53 +54,58 @@ export default function Mapbox() {
 
   const handleMapClick = (event: MapMouseEvent) => {
     const { lng, lat } = event.lngLat;
-    const { x: cursorX, y: cursorY } = event.point;
+    // const { x: cursorX, y: cursorY } = event.point;
     setLocation({ lat, lng });
     handleViewportChange({
       latitude: lat,
       longitude: lng,
-      zoom: viewport.zoom,
+      zoom: 16,
     });
-    setShowMap(false);
+    setShowMarker(true);
+    // setShowMap(false);
   };
-
-  const handleMapLoad = (map: any) => {
-    setMapInstance(map);
+  const handleMapLoad = () => {
+    // setMapInstance(map);
     if (mapRef.current) {
       mapRef.current.addControl(
         new MapboxGeocoder({
           accessToken: MAPBOX_TOKEN,
           mapboxgl: mapboxgl,
           marker: true,
-          zoom: viewport.zoom
+          zoom: viewport.zoom,
         }),
-        'top-left'
+        "top-left"
       );
       mapRef.current.addControl(new mapboxgl.NavigationControl());
     }
   };
 
   return (
-    <div className={styles.mapOverlay}>
-      <div className={styles.mapContainer}>
-        <MapGL
-          ref={mapRef}
-          mapboxAccessToken={MAPBOX_TOKEN}
-          initialViewState={{
-            longitude: 0,
-            latitude: 0,
-            zoom: 1
-          }}
-          style={{ width: '100%', height: '100%' }}
-          mapStyle="mapbox://styles/mapbox/standard-satellite"
-          onClick={handleMapClick}
-          onLoad={handleMapLoad}
-        >
-          {location.lat !== 0 && location.lng !== 0 && (
-            <Marker longitude={location.lng} latitude={location.lat} color="red" />
-          )}
-        </MapGL>
+    <>
+      {/* <div>{MAPBOX_TOKEN}</div> */}
+      <div className={styles.mapOverlay}>
+        <div className={styles.mapContainer}>
+          {
+            <MapGL
+              ref={mapRef}
+              mapboxAccessToken={MAPBOX_TOKEN}
+              initialViewState={viewport}
+              style={{ height: "30vw", width: "50vw" }}
+              mapStyle="mapbox://styles/mapbox/standard-satellite"
+              onClick={handleMapClick}
+              onLoad={handleMapLoad}
+            >
+              {showMarker && (
+                <Marker
+                  longitude={location.lng}
+                  latitude={location.lat}
+                  color="red"
+                />
+              )}
+            </MapGL>
+          }
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,23 +1,24 @@
 'use client';
 
-import { Suspense } from 'react';
-import { useSearchParams } from 'next/navigation';
+import { Suspense, useState } from 'react';
+// import { useSearchParams } from 'next/navigation';
 import styles from '../styles/main.module.css';
 import Mapbox from '@/components/mapbox';
+import Drawer from '@/components/drawer';
 
 const MainContent = () => {
-  const searchParams = useSearchParams();
-  const username = searchParams?.get('username');
-  const lat = searchParams?.get('lat');
-  const lng = searchParams?.get('lng');
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  // const searchParams = useSearchParams();
+  // const username = searchParams?.get('username');
+  // const lat = searchParams?.get('lat');
+  // const lng = searchParams?.get('lng');
+
+  const toggleDrawer = () => setIsDrawerOpen(!isDrawerOpen);
 
   return (
     <div className={styles.mainPage}>
-      {/* <h1>Welcome to Landsat Explorer</h1>
-      <div className={styles.userInfo}>
-        <p>Username: {username}</p>
-        <p><strong>Selected Location:</strong> Latitude: {lat}, Longitude: {lng}</p>
-      </div> */}
+      <Drawer isOpen={isDrawerOpen} onToggle={toggleDrawer} />
+      <h1>Welcome to Landsat Explorer</h1>
       <Mapbox />
     </div>
   );
