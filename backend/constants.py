5b94cbf4435fca24b62ceffcd060cbedfca79cb2
walkerhsu <walkerhsu0808@gmail.com>
2024-10-06 04:18:17 +0800

    ✨ New_map_APIs

--- backend/constants.py
+++ backend/constants.py
@@ -1,14 +1,12 @@
 SERVICE_ACCOUNT_KEY = './secret/earth-engine-SA-key.json'
 PIXEL_SIZE = 30
 COLLECTIONS = [
-    "LANDSAT/LC08/C02/T1_TOA",
-    "LANDSAT/LC09/C02/T1_TOA",
     "LANDSAT/LC08/C02/T1_L2",
     "LANDSAT/LC09/C02/T1_L2",
 ]
 
 L2_BANDS = [
-    # "SR_B1",
+    "SR_B1",
     "SR_B2",
     "SR_B3",
     "SR_B4",
@@ -24,24 +22,3 @@ L2_BANDS = [
     # "ST_QA",
     # "ST_TRAD",
 ]
-
-TOA_BANDS = [
-    # "B1",
-    "B2",
-    "B3",
-    "B4",
-    "B5",
-    "B6",
-    "B7",
-    # "B8",
-    # "B9",
-    "B10",
-    # "B11",
-]
-
-COLLECTION_2_BANDS = {
-    "LANDSAT/LC08/C02/T1_TOA": TOA_BANDS,
-    "LANDSAT/LC09/C02/T1_TOA": TOA_BANDS,
-    "LANDSAT/LC08/C02/T1_L2": L2_BANDS,
-    "LANDSAT/LC09/C02/T1_L2": L2_BANDS,
-}
--- backend/utils/get_google_band_data.py
+++ backend/utils/get_google_band_data.py
@@ -1,14 +1,16 @@
 # import math
 import asyncio
 import math
+import queue
 from typing import List
 import ee
 from ee import Image
 from google.oauth2.service_account import Credentials
 from google.auth.transport.requests import Request
 from pyproj import Transformer, CRS
-from user_data import Feature, Geometry, LatLng, GeoJson
-from constants import COLLECTIONS, COLLECTION_2_BANDS, L2_BANDS, TOA_BANDS
+from user_data import CloudCoverage, Feature, Geometry, LatLng, GeoJson
+from constants import COLLECTIONS, L2_BANDS
+
 
 
 class LandsatGridAnalyzer:
@@ -16,6 +18,9 @@ class LandsatGridAnalyzer:
         self.default_pixel_size = pixel_size
         self.pixel_coordinates = []
         self.pixel_corners = []
+        self.memory_cache = {}
+        self.memory_cache_size = 1000
+        self.cache_queue = queue.Queue(self.memory_cache_size)
         self.service_account_key = service_account_key
         self.initialize_ee()
 
@@ -27,63 +32,97 @@ class LandsatGridAnalyzer:
         credentials.refresh(Request())
         ee.Initialize(credentials)
 
-    async def process_all_corners(
-        self, collection_name, parsed_locations: List[LatLng]
-    ) -> List[GeoJson]:
-        tasks = [
-            self.get_corners(collection_name, location) for location in parsed_locations
-        ]
-        return await asyncio.gather(*tasks)
+    def get_cache_key(self, datasetID, center: LatLng):
+        return f"{datasetID}_{center.lat}_{center.lng}"
+    
+    def put_cache_element(self, cache_key, all_SR_data):
+        self.memory_cache[cache_key] = all_SR_data
+        if self.cache_queue.qsize() > self.memory_cache_size:
+            old_data = self.cache_queue.get()
+            def removekey(d, key):
+                r = dict(d)
+                del r[key]
+                return r
+            self.memory_cache = removekey(self.memory_cache, old_data)
 
-    async def get_corners(self, collection_name, parsed_location: LatLng):
-        center_point = ee.Geometry.Point(parsed_location.lng, parsed_location.lat)
-        image = ee.ImageCollection(collection_name).filterBounds(center_point).first()
+    def process_all_corners(
+        self, collection_name, parsed_location: LatLng
+    ) -> GeoJson:
+        tasks = self.get_corners(collection_name, parsed_location)
+        return tasks
+    
+    def get_image_info(self, image):
+        first_band = ee.List([image.bandNames().get(0)])
+        single_band_image = image.select(first_band)
+        proj = single_band_image.projection()
+        return ee.Feature(
+            None,
+            {"crs": proj.crs(), "scale": proj.nominalScale()},
+        )
 
-        def get_image_info(image):
-            proj = image.projection()
-            return ee.Feature(
-                None,
-                {"crs": proj.crs().getInfo(), "scale": proj.nominalScale().getInfo()},
-            )
+    def get_corners(self, datasetID, parsed_location: LatLng):
+        image = ee.Image(datasetID)
 
-        image_data = get_image_info(image).getInfo()
-
+        image_data = self.get_image_info(image).getInfo()
         # Calculate 9 grid corners
-        grid_corners = self.calculate_grid_corners(
+        grid_corners, centers = self.calculate_grid_corners(
             image_data["properties"], parsed_location
         )
+        print(grid_corners)
+        features = []
+        all_SR_data = []
+        is_cached = False
+        cache_key = self.get_cache_key(datasetID, parsed_location)
+        if cache_key in self.memory_cache:
+            is_cached = True
+
+        for idx, (center, grid_corner) in enumerate(zip(centers, grid_corners)):
+            if is_cached:
+                SR_data = self.memory_cache[cache_key][idx]
+            else:
+                SR_data = self.get_pixel_data(datasetID, center[0], center[1])
+                all_SR_data.append(SR_data)
+            features.append(Feature(
+                type="Feature",
+                properties={"SR_data": SR_data},
+                geometry=Geometry(type="Polygon", coordinates=[grid_corner]),
+            ))
+        if not is_cached:
+            self.put_cache_element(cache_key, all_SR_data)
+        
         # create the geojson with 9 corners
         geojson = GeoJson(
             type="FeatureCollection",
-            features=[
-                Feature(
-                    type="Feature",
-                    properties={"lat": parsed_location.lat, "lng": parsed_location.lng},
-                    geometry=Geometry(type="Polygon", coordinates=grid_corners),
-                )
-            ],
+            features=features
         )
         return geojson
-
-    def calculate_grid_corners(self, image_props, center: LatLng):
-        # Create transformers based on the image CRS
+    
+    def calculate_9_centers(self, image_props, center: LatLng):
+        # Convert center to image CRS
         image_crs = CRS.from_string(image_props["crs"])
         wgs84 = CRS.from_epsg(4326)
         image_to_wgs84 = Transformer.from_crs(image_crs, wgs84, always_xy=True)
         wgs84_to_image = Transformer.from_crs(wgs84, image_crs, always_xy=True)
-
-        # Convert center to image CRS
-        center_x, center_y = wgs84_to_image.transform(center.lng, center.lat)
-
-        # Get pixel size
         pixel_size = image_props.get("scale", self.default_pixel_size)
+        center_x, center_y = wgs84_to_image.transform(center.lng, center.lat)
+        centers = []
+        for y in range(-1, 2):
+            for x in range(-1, 2):
+                pixel_center_x = center_x + (x * pixel_size)
+                pixel_center_y = center_y + (y * pixel_size)
+                center_lon, center_lat = image_to_wgs84.transform(pixel_center_x, pixel_center_y)
+                centers.append([center_lat, center_lon])
+        return center_x, center_y, centers, pixel_size, image_to_wgs84
+
+    def calculate_grid_corners(self, image_props, center: LatLng):
+        # Create transformers based on the image CRS
+        center_x, center_y, centers, pixel_size, image_to_wgs84 = self.calculate_9_centers(image_props, center)
 
         grid_corners = []
         for y in range(-1, 2):
             for x in range(-1, 2):
                 pixel_center_x = center_x + (x * pixel_size)
                 pixel_center_y = center_y + (y * pixel_size)
-
                 corners = []
                 for dy, dx in [(-0.5, -0.5), (-0.5, 0.5), (0.5, 0.5), (0.5, -0.5)]:
                     corner_x = pixel_center_x + (dx * pixel_size)
@@ -94,7 +133,8 @@ class LandsatGridAnalyzer:
 
                 grid_corners.append(corners)
 
-        return grid_corners
+        return grid_corners, centers
+
     def process_collection(self, collection, collection_name):
         def process_image(image):
             return ee.Feature(
@@ -112,12 +152,14 @@ class LandsatGridAnalyzer:
 
         return collection.map(process_image)
 
-    async def get_dataset(self, lat, lng, start_date, end_date):
+    async def get_dataset(self, lat, lng, start_date, end_date, cloud_coverage: CloudCoverage):
         center_point = ee.Geometry.Point(lng, lat)
         collections = [
             ee.ImageCollection(collection_name)
             .filterBounds(center_point)
             .filterDate(start_date, end_date)
+            .filter(ee.Filter.lte("CLOUD_COVER", cloud_coverage.max))
+            .filter(ee.Filter.gte("CLOUD_COVER", cloud_coverage.min))
             .sort("system:time_start")
             for collection_name in COLLECTIONS
         ]
@@ -130,7 +172,11 @@ class LandsatGridAnalyzer:
         result = all_features.getInfo()
 
         dataset_list = [
-            {"collection_name": collection_name, "dataset_id": []}
+            {
+                "collection_name": collection_name,
+                "location": {"lat": lat, "lng": lng},
+                "dataset_id": [],
+            }
             for collection_name in COLLECTIONS
         ]
         for feature in result["features"]:
@@ -149,47 +195,27 @@ class LandsatGridAnalyzer:
 
         return dataset_list
 
-    async def process_all_locations(self, locations, start_date, end_date):
-        tasks = [
-            self.get_dataset(loc.lat, loc.lng, start_date, end_date)
-            for loc in locations
-        ]
-        return await asyncio.gather(*tasks)
+    async def process_all_locations(self, locations, start_date, end_date, cloud_coverage):
+        tasks = []
+        for loc in locations:
+            tasks.extend(await self.get_dataset(loc.lat, loc.lng, start_date, end_date, cloud_coverage))
+
+        return tasks
 
     def get_pixel_data(self, datasetID, lat, lon):
-        bands = []
-        for collection in COLLECTION_2_BANDS.keys():
-            if collection in datasetID:
-                bands = COLLECTION_2_BANDS[collection]
-                break
-        scene_bands = ee.Image(datasetID).select(bands)
+        scene_bands = ee.Image(datasetID).select(L2_BANDS)
         point = ee.Geometry.Point(lon, lat)
         band_values = scene_bands.reduceRegion(
             geometry=point, reducer=ee.Reducer.mean(), scale=1, tileScale=16
         ).getInfo()
-        if bands == L2_BANDS:
-            print("L2 Bands")
-            blue = band_values["SR_B2"]
-            green = band_values["SR_B3"]
-            red = band_values["SR_B4"]
-            nir = band_values["SR_B5"]
-            swir = band_values["SR_B6"]
-            swir2 = band_values["SR_B7"]
-            tirs1 = band_values["ST_B10"]
-            temp = self.get_temp_data(tirs1)
-        elif bands == TOA_BANDS:
-            print("TOA Bands")
-            max_value = 65535
-            blue = band_values["B2"] * max_value
-            green = band_values["B3"] * max_value
-            red = band_values["B4"] * max_value
-            nir = band_values["B5"] * max_value
-            swir = band_values["B6"] * max_value
-            swir2 = band_values["B7"] * max_value
-            temp = None
-        else:
-            print("No bands found")
-        print(red, green, blue, nir, swir)
+        blue = band_values["SR_B2"]
+        green = band_values["SR_B3"]
+        red = band_values["SR_B4"]
+        nir = band_values["SR_B5"]
+        swir = band_values["SR_B6"]
+        swir2 = band_values["SR_B7"]
+        tirs1 = band_values["ST_B10"]
+        temp = self.get_temp_data(tirs1)
         SR_data = {
             "color": self.get_RGB_data(red, green, blue, nir, swir),
             "ndvi": self.get_ndvi(nir, red),
@@ -203,16 +229,21 @@ class LandsatGridAnalyzer:
             "temperature": temp
         }
         return SR_data
-    
-    def download_datasets(self, datasetIDs, lat, lng):
-        SR_data = []
-        for datasetID in datasetIDs:
-            SR_data.append({
-                "lat": lat,
-                "lng": lng,
-                "SR_data": self.get_pixel_data(datasetID, lat, lng)
-            })
-        return SR_data
+
+    def download_dataset(self, datasetID, location: LatLng):
+        cache_key = self.get_cache_key(datasetID, location)
+        if cache_key in self.memory_cache:
+            return self.memory_cache[cache_key]
+
+        image = ee.Image(datasetID)
+        image_data = self.get_image_info(image).getInfo()
+
+        _, _, centers, _, _ = self.calculate_9_centers(image_data["properties"], location)
+        all_SR_data = []
+        for center in centers:
+            all_SR_data.append(self.get_pixel_data(datasetID, center[0], center[1]))
+        self.put_cache_element(cache_key, all_SR_data)
+        return all_SR_data
 
     def get_RGB_data(self, red, green, blue, nir, swir):
         # Apply custom processing
@@ -225,8 +256,7 @@ class LandsatGridAnalyzer:
         red = int(((red / max_value) ** (1 / gamma)) * 255)
         green = int(((green / max_value) ** (1 / gamma)) * 255)
         blue = int(((blue / max_value) ** (1 / gamma)) * 255)
-
-        return {"red": red, "green": green, "blue": blue}
+        return f"#{red:02x}{green:02x}{blue:02x}"
 
     def get_ndvi(self, nir, red):
         return (nir - red) / (nir + red)
@@ -254,5 +284,5 @@ class LandsatGridAnalyzer:
 
     def get_temp_data(self, st_b10):
         if st_b10 is not None and st_b10 != 0:  # Check for valid data
-            return (st_b10 * 0.00341802 + 149.0)  # Scale factor and offset
+            return st_b10 * 0.00341802 + 149.0  # Scale factor and offset
         return 0.0
