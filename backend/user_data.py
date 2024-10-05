ba7746435ce5336feb23888807f353dafb1081f7
Markymark <mark.pc.lee@gmail.com>
2024-10-05 14:29:43 +0800

    update user data template

--- backend/user_data.py
+++ backend/user_data.py
@@ -11,11 +11,17 @@ class Location(BaseModel):
     latlng: LatLon
     dataset: str
     addedDate: str
+    
+class Detail(BaseModel):
+    label: str
+    field: str
 
 class UserData(BaseModel):
     id: str
     name: str
     avatarUrl: str
     email: str
+    phone: str
+    details: list[Detail]
     locationHistory: list[Location]
     
