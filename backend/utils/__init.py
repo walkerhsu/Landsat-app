b39bcb474ee907e8822c683bf81cfb28ba04befa
Walker <83209083+walkerhsu@users.noreply.github.com>
2024-10-06 17:49:45 +0800

    🐛 Path incorrect bug fixed (#29)

--- backend/utils/__init.py
+++ backend/utils/__init.py
@@ -1,3 +1,3 @@
 from utils.firebase import db
-from backend.utils.get_landsat_location import get_landsat_location
-from backend.utils.get_landsat_location import next_landsat_time
+from utils.get_landsat_location import get_landsat_location
+from utils.get_landsat_location import next_landsat_time
